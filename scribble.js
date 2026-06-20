3c755b1bce53b27890a6268c6fec56dba738d4de
hannahmathan <hannahmathan00@gmail.com>
2026-06-20 18:22:28 +0530

    Create scribble.js

---
+++ scribble.js
@@ -0,0 +1,44 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth - 40;
+canvas.height = window.innerHeight - 220;
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => drawing = true);
+
+canvas.addEventListener("mouseup", () => {
+    drawing = false;
+    ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+function draw(e){
+
+    if(!drawing) return;
+
+    ctx.lineWidth =
+        document.getElementById("brushSize").value;
+
+    ctx.strokeStyle =
+        document.getElementById("colorPicker").value;
+
+    ctx.lineCap="round";
+
+    ctx.lineTo(e.offsetX,e.offsetY);
+
+    ctx.stroke();
+
+    ctx.beginPath();
+
+    ctx.moveTo(e.offsetX,e.offsetY);
+
+}
+
+function clearCanvas(){
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+}
